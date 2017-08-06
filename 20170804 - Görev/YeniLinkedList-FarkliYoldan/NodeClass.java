eaa00e6ab82d039f70be2c77f90437d45a75a35b
Samet Yılmaz <blizzardy.10@gmail.com>
2017-08-06 23:38:26 +0300

    Add files via upload

---
+++ 20170804 - Görev/YeniLinkedList-FarkliYoldan/NodeClass.java
@@ -0,0 +1,22 @@
+
+public class NodeClass {
+
+	String isim;
+	NodeClass next;
+	
+	public String getIsim() {
+		return isim;
+	}
+
+	public void setIsim(String isim) {
+		this.isim = isim;
+	}
+
+	public NodeClass getNext() {
+		return next;
+	}
+
+	public void setNext(NodeClass next) {
+		this.next = next;
+	}
+}
